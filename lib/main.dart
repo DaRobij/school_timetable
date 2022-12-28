b2fddf002d8866fe8b4449f9f971c6fb99ed14ce
DaRobij <robert.valentino.ciobanu@gmail.com>
2022-12-28 12:02:49 +0200

    aaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("ABC13")),
+      appBar: AppBar(title: const Text("aaaaAaa")),
       //o sa in viitor text custom
     );
   }
